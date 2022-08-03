77891eba2e86b7ea592c876bc4891a556b16cc25
aenisyifaa <syifaeni190@gmail.com>
2022-08-03 11:51:57 +0700

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Intro Codeigniter 3</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Intro Codeigniter 3</h1>
+
+	<center>
+		<table width="600px" height="100%" border="1">
+			<tbody>
+				<tr height="100px">
+					<td width="100px" style="background-color: red;">Warna</td>
+				</tr>
+				<tr height="30px">
+					<td width="100px">Beranda Profile Berita</td>
+				</tr>
+				<tr height="500px">
+					<td width="100px"></td>
+				</tr>
+				<tr height="30px">
+					<td width="100px">@2022</td>
+				</tr>
+	</table>
+	</center>
+</div>
+
+</body>
+</html>
